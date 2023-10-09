589868d40e044ecb8b81c3ced9ca738c74547dea
tuanthanh-2 <133196783+tuanthanh-2@users.noreply.github.com>
2023-10-09 15:33:01 +0700

    new update 3:22 9/10/2023

--- vuejs/src/main.js
+++ vuejs/src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
 import Antd from 'ant-design-vue';
 
-import clientRouter from './client/router';
-import adminRouter from './admin/router';
+import router from './router';
+
 
 
 
@@ -13,17 +13,23 @@ import App from './App.vue';
 
 
 
+
 import 'bootstrap/dist/css/bootstrap-grid.min.css';
 import 'bootstrap/dist/css/bootstrap-utilities.min.css';
 import 'ant-design-vue/dist/antd.css';
 
 
 
+
 const app = createApp(App);
 
-app.use(clientRouter)
+app.use(router)
 app.use(Antd)
 app.mount('#client-app');
+
+
+
+
 
 
 
--- vuejs/src/router/index.js
+++ vuejs/src/router/index.js
@@ -7,28 +7,28 @@ const router = createRouter({
         {
             path: '/',
             name: 'client',
-            component: () => import('../views/trang-chu/trang-chu.vue')
+            component: () => import('../client/views/trang-chu/trang-chu.vue')
         },
       
         {
             path: '/cho-canh',
-            component: () => import('../views/cho-canh/cho-canh.vue')
+            component: () => import('../client/views/cho-canh/cho-canh.vue')
         },
         {
             path: '/meo-canh',
-            component: () => import('../views/meo-canh/meo-canh.vue')
+            component: () => import('../client/views/meo-canh/meo-canh.vue')
         },
         {
             path: '/phu-kien-thu-cung',
-            component: () => import('../views/phu-kien-thu-cung/phu-kien-thu-cung.vue')
+            component: () => import('../client/views/phu-kien-thu-cung/phu-kien-thu-cung.vue')
         },
         {
             path: '/dich-vu-ho-tro',
-            component: () => import('../views/dich-vu-ho-tro/dich-vu-ho-tro.vue')
+            component: () => import('../client/views/dich-vu-ho-tro/dich-vu-ho-tro.vue')
         },
         {
             path: '/pho-bien-nhat',
-            component: () => import('../views/pho-bien-nhat/pho-bien-nhat.vue')
+            component: () => import('../client/views/pho-bien-nhat/pho-bien-nhat.vue')
         },
         
        
