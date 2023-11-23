29f0d67182337c0991ecc621c94c229696ca2d3d
tuanthanh2603 <tuanthanhktltk21@gmail.com>
2023-11-23 15:30:19 +0700

    commit by tuanthanh 23/11/2023

--- vuejs/src/client/views/cho-canh/cho-canh.vue
+++ vuejs/src/client/views/cho-canh/cho-canh.vue
@@ -2,7 +2,7 @@
     <a-page-header style="border: 1px solid rgb(235, 237, 240)" title="Chó cảnh" @back="() => $router.go(-1)" />
     <div class="container">
        
-        <div style="margin-top: 1em; margin-bottom: 5em;">
+        <!-- <div style="margin-top: 1em; margin-bottom: 5em;">
             <div style="box-shadow: 0 3px 13px rgb(204, 204, 214); padding: 25px 30px; border-radius: 8px; background: #fff;">
                 <div class="row g-5">
                     <div class="col-md-4">
@@ -67,173 +67,99 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> -->
         <div class="row g-5">
             <div class="col-md-3 col-sm-6">
                 <div class="product-grid">
-                    <div class="card-container">
-                        <div class="cat-face">
-                            <div class="circle">
-                                <img src="https://azpet.com.vn/wp-content/uploads/2021/06/Alaska.jpg">
+                    <div class="card">
+                        <div class="background-dog">
+                            <div class="image-dog">
+                                <img src="https://azpet.com.vn/wp-content/uploads/2021/06/Alaska.jpg" >
                             </div>
                         </div>
-
-                    </div>
-                </div>
-            </div>
-
-            <!-- <div class="col-md-3 col-sm-6">
-                <div class="product-grid">
-                    <div class="product-image">
-                        <a href="#" class="image">
-                            <img src="https://azpet.com.vn/wp-content/uploads/2021/06/Alaska.jpg">
-                        </a>
-                        <span class="product-discount-label">-23%</span>
-                        <ul class="product-links">
-                            <li><a href="#"><i class="fa fa-search"></i></a></li>
-                            <li><a href="#"><i class="fa fa-heart"></i></a></li>
-                            <li><a href="#"><i class="fa fa-random"></i></a></li>
-                        </ul>
-                        <a href="" class="add-to-cart">Thêm vào giỏ hàng</a>
-                    </div>
-                    <div class="product-content">
-                        <h3 class="title"><a href="#">Chó</a></h3>
-                        <div class="price">1.800.000đ <span>2.000.000đ</span></div>
                     </div>
                 </div>
             </div>
 
             <div class="col-md-3 col-sm-6">
                 <div class="product-grid">
-                    <div class="product-image">
-                        <a href="#" class="image">
-                            <img src="https://azpet.com.vn/wp-content/uploads/2021/06/Alaska.jpg">
-                        </a>
-                        <span class="product-discount-label">-3%</span>
-                        <ul class="product-links">
-                            <li><a href="#"><i class="fa fa-search"></i></a></li>
-                            <li><a href="#"><i class="fa fa-heart"></i></a></li>
-                            <li><a href="#"><i class="fa fa-random"></i></a></li>
-                        </ul>
-                        <a href="" class="add-to-cart">Thêm vào giỏ hàng</a>
-                    </div>
-                    <div class="product-content">
-                        <h3 class="title"><a href="#">Chó</a></h3>
-                        <div class="price">1.800.000đ <span>2.000.000đ</span></div>
+                    <div class="card">
+                        <div class="background-dog">
+                            <div class="image-dog">
+                                <img src="https://azpet.com.vn/wp-content/uploads/2021/06/Alaska.jpg" >
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
 
             <div class="col-md-3 col-sm-6">
                 <div class="product-grid">
-                    <div class="product-image">
-                        <a href="#" class="image">
-                            <img src="https://azpet.com.vn/wp-content/uploads/2021/06/Alaska.jpg">
-                        </a>
-                        <span class="product-discount-label">-0%</span>
-                        <ul class="product-links">
-                            <li><a href="#"><i class="fa fa-search"></i></a></li>
-                            <li><a href="#"><i class="fa fa-heart"></i></a></li>
-                            <li><a href="#"><i class="fa fa-random"></i></a></li>
-                        </ul>
-                        <a href="" class="add-to-cart">Thêm vào giỏ hàng</a>
-                    </div>
-                    <div class="product-content">
-                        <h3 class="title"><a href="#">Chó</a></h3>
-                        <div class="price">1.800.000đ <span>2.000.000đ</span></div>
+                    <div class="card">
+                        <div class="background-dog">
+                            <div class="image-dog">
+                                <img src="https://azpet.com.vn/wp-content/uploads/2021/06/Alaska.jpg" >
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
 
             <div class="col-md-3 col-sm-6">
                 <div class="product-grid">
-                    <div class="product-image">
-                        <a href="#" class="image">
-                            <img src="https://azpet.com.vn/wp-content/uploads/2021/06/Alaska.jpg">
-                        </a>
-                        <span class="product-discount-label">-0%</span>
-                        <ul class="product-links">
-                            <li><a href="#"><i class="fa fa-search"></i></a></li>
-                            <li><a href="#"><i class="fa fa-heart"></i></a></li>
-                            <li><a href="#"><i class="fa fa-random"></i></a></li>
-                        </ul>
-                        <a href="" class="add-to-cart">Thêm vào giỏ hàng</a>
-                    </div>
-                    <div class="product-content">
-                        <h3 class="title"><a href="#">Chó</a></h3>
-                        <div class="price">1.800.000đ <span>2.000.000đ</span></div>
+                    <div class="card">
+                        <div class="background-dog">
+                            <div class="image-dog">
+                                <img src="https://azpet.com.vn/wp-content/uploads/2021/06/Alaska.jpg" >
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-
-            <div class="col-md-3 col-sm-6">
-                <div class="product-grid">
-                    <div class="product-image">
-                        <a href="#" class="image">
-                            <img src="https://azpet.com.vn/wp-content/uploads/2021/06/Alaska.jpg">
-                        </a>
-                        <span class="product-discount-label">-0%</span>
-                        <ul class="product-links">
-                            <li><a href="#"><i class="fa fa-search"></i></a></li>
-                            <li><a href="#"><i class="fa fa-heart"></i></a></li>
-                            <li><a href="#"><i class="fa fa-random"></i></a></li>
-                        </ul>
-                        <a href="" class="add-to-cart">Thêm vào giỏ hàng</a>
-                    </div>
-                    <div class="product-content">
-                        <h3 class="title"><a href="#">Chó</a></h3>
-                        <div class="price">1.800.000đ <span>2.000.000đ</span></div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-3 col-sm-6">
-                <div class="product-grid">
-                    <div class="product-image">
-                        <a href="#" class="image">
-                            <img src="https://azpet.com.vn/wp-content/uploads/2021/06/Alaska.jpg">
-                        </a>
-                        <span class="product-discount-label">-0%</span>
-                        <ul class="product-links">
-                            <li><a href="#"><i class="fa fa-search"></i></a></li>
-                            <li><a href="#"><i class="fa fa-heart"></i></a></li>
-                            <li><a href="#"><i class="fa fa-random"></i></a></li>
-                        </ul>
-                        <a href="" class="add-to-cart">Thêm vào giỏ hàng</a>
-                    </div>
-                    <div class="product-content">
-                        <h3 class="title"><a href="#">Chó</a></h3>
-                        <div class="price">1.800.000đ <span>2.000.000đ</span></div>
-                    </div>
-                </div>
-            </div> -->
 
         </div>
     </div>
 </template>
+<style scoped>
+.card {
+    width: 300px;
+    height: 400px;
+    border-radius: 53px;
+    background-color: #EE9CB2;
+}
+.background-dog{
+    background-image: url(../../../../public/images/bg_dog.png);  
+    background-size: cover; 
+    background-position: center; 
+    background-repeat: no-repeat; 
+    width: 89%;
+    height: 70%;
+    margin: 0 auto;
+}
+.background-dog .image-dog {
+    height: 80%;
+    width: 80%;
+    margin: auto;
+    overflow: hidden;
+    
+}
+.background-dog .image-dog img {
+    width: 65%;
+    height: 60%;
+    margin: 0 auto;
+    object-fit: cover;
+    margin-top: 35%;
+    
+}
+</style>
 
 <script>
 import { defineComponent, ref, watch } from 'vue';
 export default defineComponent({
     setup() {
-        const options1 = ref([{
-            value: 'Dài',
-            label: 'Dài',
-        }, {
-            value: 'Ngắn',
-            label: 'Ngắn',
-        }]);
-        const value = ref([]);
-        const handleChange = value => {
-            console.log(`selected ${value}`);
-        };
-        watch(value, () => {
-            console.log('value', value.value);
-        });
+        
         return {
-            options1,
-            handleChange,
-            value,
+            
         }
     }
     
