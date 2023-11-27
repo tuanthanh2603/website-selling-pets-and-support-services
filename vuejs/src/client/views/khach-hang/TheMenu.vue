a535eef34bdf8c56b9a01e8e24017adf0bb5fb5c
tuanthanh2603 <tuanthanhktltk21@gmail.com>
2023-11-27 14:02:48 +0700

    commit by tuanthanh

---
+++ vuejs/src/client/views/khach-hang/TheMenu.vue
@@ -0,0 +1,58 @@
+<template>
+    <a-menu style="width: 100%" mode="vertical">
+        <a-menu-item key="1">
+            <template #icon>
+
+            </template>
+            Thông tin tài khoản
+        </a-menu-item>
+        <a-menu-item key="2">
+            <template #icon>
+
+            </template>
+            Yêu thích
+        </a-menu-item>
+        <a-menu-item key="3">
+            <template #icon>
+
+            </template>
+            Giỏ hàng
+        </a-menu-item>
+        <a-menu-item key="4">
+            <template #icon>
+
+            </template>
+            Nhật ký thú cưng
+        </a-menu-item>
+        <a-menu-item key="6">
+            <template #icon>
+
+            </template>
+            Trò chuyện
+        </a-menu-item>
+        <a-menu-item key="7">
+            <template #icon>
+
+            </template>
+            Lịch sử đơn hàng
+        </a-menu-item>
+        <a-menu-item key="8">
+            <template #icon>
+
+            </template>
+            Sổ địa chỉ nhận hàng
+        </a-menu-item>
+
+    </a-menu>
+</template>
+<script>
+import { defineComponent, reactive } from 'vue';
+export default defineComponent({
+    components: {
+
+    },
+    setup() {
+
+    }
+})
+</script>
--- vuejs/src/client/views/khach-hang/thong-tin-tai-khoan.vue
+++ vuejs/src/client/views/khach-hang/thong-tin-tai-khoan.vue
@@ -1,16 +1,33 @@
 <template>
-    gg
+    <div class="container">
+        <div class="row">
+            <div class="col-md-2">
+                <TheMenu />
+            </div>
+            <div class="col-md-10">
+                <div class="container mt-3">
+                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                        <h6 class="h3 mb-0 text-gray-800" style="font-size: 22px;">Thông tin tài khoản</h6>
+                    </div>
+                    <div class="container">
+                        2
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 <script>
-
+import TheMenu from './TheMenu.vue';
 import { defineComponent, ref } from 'vue';
 export default defineComponent ({
     components: {
+        TheMenu
 
     },
     setup(){
         return{
-            
+
         }
     }
 })
