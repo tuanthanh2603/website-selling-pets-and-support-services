044fbb3eb2ae04e522b644806a446c98607b277f
Bearyeiz <giaphuoc.work@gmail.com>
2023-12-02 22:03:33 +0700

    update

---
+++ backend/controllers/client/pServiceController.js
@@ -0,0 +1,5 @@
+import { ProcessService } from "../../models/processServiceModel.js";
+
+export const addProcessService = async (req, res) => {
+    console.log(req.body)
+}
--- backend/models/processServiceModel.js
+++ backend/models/processServiceModel.js
@@ -5,5 +5,15 @@ const processService = new Scheme({
     status: {
         type: String,
         required: true,
+    },
+    created_at: {
+        type: Date,
+        default: Date.now,
+    },
+    employee_id: {
+        type: String,
+        require: true,
     }
-})
+});
+
+export const ProcessService = mongoose.model("ProcessService", processService, "ProcessService");
